4a6df65172fc28e2ffc0a8bfe9d3d6ff772b0c41
Seby B Joseph <sebybjoseph2009@gmail.com>
2023-12-10 23:45:37 +0530

    Add solution for times.js

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,17 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+    //const startTime = new Date();
+    const startTime = performance.now();
+    let sum = 0;
+    for (let i = 0; i < n; i++) {
+      sum += i;
+    }
+    //const endTime = new Date();
+    const endTime = performance.now();
+  
+
+    // Return the elapsed time
+    return endTime - startTime;
+
 }
